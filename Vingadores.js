283250d0624c7dee2ea9ae50a2f84819e9a9c63d
LNaito <129390918+LNaito@users.noreply.github.com>
2025-08-21 21:28:05 -0300

    Add files via upload

---
+++ Vingadores.js
@@ -0,0 +1,219 @@
+const Thanos = {
+    nome: "Thanos",
+    codinome: "O Tita Louco",
+    armaPrincipal: "Manopla do Infinito",
+    armaSecundaria: "Joias do Infinito",
+    velocidade: 65, //considere de 0 a 100
+    forca: 100, //considere de 0 a 100
+    resistencia: 100, //considere de 0 a 100
+    descricao: function () {
+        return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de Forca: " + this.forca + "\n"
+        + "Nivel de Velocidade: " + this.velocidade + "\n"
+        + "Nivel de Resistencia: " + this.resistencia
+    }
+}
+
+const HomemdeFerro = {
+    nome: "Tony Stark",
+    codinome: "Homem de Ferro",
+    armaPrincipal: "Armadura",
+    armaSecundaria: "",
+    velocidade: 60, //considere de 0 a 100
+    forca: 70, //considere de 0 a 100
+    resistencia: 80, //considere de 0 a 100
+    descricao: function () {
+        return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de Forca: " + this.forca + "\n"
+        + "Nivel de Velocidade: " + this.velocidade + "\n"
+        + "Nivel de Resistencia: " + this.resistencia
+    }
+}
+
+const Thor = {
+    nome: "Thor Odinson",
+    codinome: "Thor",
+    armaPrincipal: "Mjolnir",
+    armaSecundaria: "",
+    velocidade: 55, //considere de 0 a 100
+    forca: 90, //considere de 0 a 100
+    resistencia: 90, //considere de 0 a 100
+    descricao: function () {
+        return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de Forca: " + this.forca + "\n"
+        + "Nivel de Velocidade: " + this.velocidade + "\n"
+        + "Nivel de Resistencia: " + this.resistencia
+    }
+}
+
+const Hulk = {
+    nome: "Bruce Banner",
+    codinome: "Hulk",
+    armaPrincipal: "Transformacao em Hulk",
+    armaSecundaria: "",
+    velocidade: 65, //considere de 0 a 100
+    forca: 100, //considere de 0 a 100
+    resistencia: 95, //considere de 0 a 100
+    descricao: function () {
+        return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de Forca: " + this.forca + "\n"
+        + "Nivel de Velocidade: " + this.velocidade + "\n"
+        + "Nivel de Resistencia: " + this.resistencia
+    }
+}
+
+const CapitaoAmerica = {
+    nome: "Steve Rogers",
+    codinome: "Capitao America",
+    armaPrincipal: "Escudo",
+    armaSecundaria: "",
+    velocidade: 85, //considere de 0 a 100
+    forca: 75, //considere de 0 a 100
+    resistencia: 80, //considere de 0 a 100
+    descricao: function () {
+        return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de Forca: " + this.forca + "\n"
+        + "Nivel de Velocidade: " + this.velocidade + "\n"
+        + "Nivel de Resistencia: " + this.resistencia
+    }
+}
+
+const ViuvaNegra = {
+    nome: "Natasha Romanov",
+    codinome: "Viuva Negra",
+    armaPrincipal: "Braceletes",
+    armaSecundaria: "Arma de fogo",
+    velocidade: 80, //considere de 0 a 100
+    forca: 55, //considere de 0 a 100
+    resistencia: 55, //considere de 0 a 100
+    descricao: function () {
+        return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de Forca: " + this.forca + "\n"
+        + "Nivel de Velocidade: " + this.velocidade + "\n"
+        + "Nivel de Resistencia: " + this.resistencia
+    }
+}
+
+const GaviaoArqueiro = {
+    nome: "Clint Barton",
+    codinome: "Gaviao Arqueiro",
+    armaPrincipal: "Arco",
+    armaSecundaria: "",
+    velocidade: 60, //considere de 0 a 100
+    forca: 60, //considere de 0 a 100
+    resistencia: 55, //considere de 0 a 100
+    descricao: function () {
+        return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de Forca: " + this.forca + "\n"
+        + "Nivel de Velocidade: " + this.velocidade + "\n"
+        + "Nivel de Resistencia: " + this.resistencia
+    }
+}
+
+const HomemFormiga = {
+    nome: "Scott Lang",
+    codinome: "Homem Formiga",
+    armaPrincipal: "Equipamento de mudanca de massa",
+    armaSecundaria: "",
+    velocidade: 65, //considere de 0 a 100
+    forca: 90, //considere de 0 a 100
+    resistencia: 75, //considere de 0 a 100
+    descricao: function () {
+        return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de Forca: " + this.forca + "\n"
+        + "Nivel de Velocidade: " + this.velocidade + "\n"
+        + "Nivel de Resistencia: " + this.resistencia
+    }
+}
+
+const Visao = {
+    nome: "Visao",
+    codinome: "Visao",
+    armaPrincipal: "Corpo robotico",
+    armaSecundaria: "",
+    velocidade: 75, //considere de 0 a 100
+    forca: 75, //considere de 0 a 100
+    resistencia: 80, //considere de 0 a 100
+    descricao: function () {
+        return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de Forca: " + this.forca + "\n"
+        + "Nivel de Velocidade: " + this.velocidade + "\n"
+        + "Nivel de Resistencia: " + this.resistencia
+    }
+}
+
+
+
+let Vingadores = [HomemFormiga, HomemdeFerro, Hulk, Thor, CapitaoAmerica, ViuvaNegra, Visao, GaviaoArqueiro, Thanos]
+let Resto = [] //Lista para ignorar
+
+
+for (heroi of Vingadores){
+    console.log(heroi)
+    for (oposto of Vingadores){
+        if (!Resto.includes(oposto) && oposto != heroi){
+        console.log(oposto, "é o oponente")
+        poder = 0
+
+            if (heroi.forca > oposto.forca) {
+                console.log (heroi.nome, "é mais forte!")
+                poder = poder + 1 
+            }
+            else (
+                console.log (oposto.nome, "é mais forte!") 
+            )
+
+            if (heroi.velocidade > oposto.velocidade) {
+                console.log (heroi.nome, "é mais rapido!")
+                poder = poder + 1 
+            }
+            else (
+                console.log (oposto.nome, "é mais rapido!") 
+            )
+        
+            if (heroi.resistencia > oposto.resistencia) {
+                console.log (heroi.nome, "é mais resistente!")
+                poder = poder + 1 
+            }
+            else (
+                console.log (oposto.nome, "é mais resistente!") 
+            )
+
+            if (poder > 1){
+                console.log(heroi.nome, "ganha!")
+            }
+            else {
+                console.log(heroi.nome, "perde pro", oposto.nome)
+            }
+        }
+    Resto.push(heroi)
+    }
+
+}
